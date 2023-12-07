c620d58f84dd63d7b8ec2d1ac995d961ebc2b907
matthieu-lefebvre <matthieulefebvre92@gmail.com>
2023-12-07 15:39:26 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-(echo n; echo p; echo 1; echo 2048; echo 33554431; echo w) | fdisk /dev/sdc
+(echo n; echo p; echo 1;  ;  ; echo w) | fdisk /dev/sdc
 mkfs.ext4 /dev/sdc1
 mkdir /mnt/TTS
 mount /dev/sdc1 /mnt/TTS
